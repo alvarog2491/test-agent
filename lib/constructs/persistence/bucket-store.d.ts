7cecb60a6e80e0212abd4bee1afa2cb256195ae4
alvarog2491 <159990212+alvarog2491@users.noreply.github.com>
2026-01-31 18:19:32 +0100

    Recreate

--- lib/constructs/persistence/bucket-store.d.ts
+++
@@ -1,8 +0,0 @@
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
-export declare class BucketStore extends Construct {
-    readonly knowledgeBaseBucket: s3.IBucket;
-    readonly logsBucket: s3.IBucket;
-    readonly evaluationsResultsBucket: s3.IBucket;
-    constructor(scope: Construct, id: string);
-}
--- lib/stacks/monitoring-stack.d.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-interface MonitoringStackProps extends cdk.StackProps {
-    readonly knowledgeBaseId: string;
-}
-export declare class MonitoringStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: MonitoringStackProps);
-}
-export {};
